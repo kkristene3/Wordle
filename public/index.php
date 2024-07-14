29207cbe07e3151aa5b568d625ca2589783edecb
kkristene3 <kkristene3@gmail.com>
2024-07-13 22:57:57 -0400

    Add php functions for getting a wordle and checking if word exists

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
+<?php
+require 'app\models\wordOfDay.php';
+use Wordle\wordOfDay;
+ 
+$wordOfDayInstance = new WordOfDay();
+$wordOfTheDay = $wordOfDayInstance->getWordOfTheDay();
+
+?>
+
+<!-- HTML CODE -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,6 +29,10 @@
 </head>
 
 <body>
+
+<h1>Word of the Day</h1>
+    <h1>Word of the Day</h1>
+    <p><?php echo htmlspecialchars($wordOfTheDay, ENT_QUOTES, 'UTF-8'); ?></p>
     <h1>Wordle</h1>
 
     <!-- Game structure -->
