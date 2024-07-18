c7c770e7845e520d4f613e79c0444f579acad4f8
Alex Oprea <90671395+thealexand@users.noreply.github.com>
2024-07-17 21:56:01 -0400

    Add PHP to index.php

--- app/models/Wordle.php
+++ app/models/Wordle.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Wordle;
 
 class Wordle{
 
--- public/index.php
+++ public/index.php
@@ -93,7 +93,64 @@
 
     <!-- JS File Location -->
     <script src="JS/index.js"></script>
+
+    
     </body>
 
 </html>
+
+<?php
+
+//connecting the file to the Wordle.php files
+require('../app/models/Wordle.php');
+
+$wordle = new Wordle();
+
+$word = $_REQUEST['word'];
+
+//grabbing the JSON file contents
+$json_content = file_get_contents('objects.json');
+
+//check if the file was read successfully
+if ($json_content === false){
+    die('Could not read JSON file');
+}
+
+//turn the JSON content into an array
+$json_array = json_decode($json_content, true);
+
+//check if the JSON was decoded properly
+if ($json_array === null){
+    die ("Error decoding the file");
+}
+
+if ($word == "resetIt"){
+}
+
+else{
+    //check if the game just started
+    if ($json_array['GAMEOVER'] == -1){
+        //if it did, get a new word and put it in the JSON
+        $wordleWord = $wordle->getWordOfTheDay();
+        $json_array['currentWord'] = $wordleWord;
+        //indicate that the game has started in the JSON
+        $json_array['GAMEOVER'] = 0;
+
+        //update the json file with the new values
+        $final_json_content = json_encode($json_array);
+        file_put_contents('objects.json', $final_json_content);
+    }
+    
+    //check if the word is valid
+    $wordle->checkWord($word);
+    
+    //if the word is valid, we check the placement
+    if ($json_array['wordValid'] == 0){
+        $wordle->checkLetterPlacement($word);
+    }
+
+    
+}
+?>
+
 
